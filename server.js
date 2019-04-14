cc8bc4492a3aca9db44dff39550f2386a78e1719
Cara Ponzini <ponzini@gmail.com>
2019-04-14 15:23:54 -0700

    updated server to handle chunking

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const url = require('url');
 
 function start(route, handle) {
   function onRequest(req, res) {
+    let postData = '';
     const pathname = url.parse(req.url).pathname;  
     console.log(`Request for ${pathname} received`);
 
-    const content = route(handle, pathname, res);
+    req.setEncoding('utf8');
+    req.addListener('data', (postDataChunk) => {
+      postData += postDataChunk;
+      console.log(`received post data chunk '${postDataChunk}'`);
+    });
 
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write(content);
-    res.end();
+    req.addListener('end', () => {
+      route(handle, pathname, res, postData);
+    });
+
   }
 
   http.createServer(onRequest).listen(8888);
