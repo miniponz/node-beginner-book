4c46affc4a3d52dcdef3c13f215e0f19419daeec
Cara Ponzini <ponzini@gmail.com>
2019-04-14 14:39:37 -0700

    updated request handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
   console.log('request handler START was called');
+  return 'Hello Start';
 }
 
 function upload() {
   console.log('request handler UPLOAD was called');
+  return 'Hello Upload';
 }
 
 exports.start = start;
