6fb86d95e6e499eadccf77a6ee84f171693ade73
Cara Ponzini <ponzini@gmail.com>
2019-04-14 14:50:53 -0700

    fixed typo in index

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ handle['/'] = requestHandlers.start;
 handle['/start'] = requestHandlers.start;
 handle['/upload'] = requestHandlers.upload;
 
-server.start(router.route);
+server.start(router.route, handle);
 
