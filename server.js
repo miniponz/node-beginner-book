2ab5aa1a7538533e54a0b281d05242898a4f0322
Cara Ponzini <ponzini@gmail.com>
2019-04-14 14:49:56 -0700

    sleep delay

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ function start(route, handle) {
     const pathname = url.parse(req.url).pathname;  
     console.log(`Request for ${pathname} received`);
 
-    route(handle, pathname);
+    const content = route(handle, pathname);
 
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World');
+    res.write(content);
     res.end();
   }
 
