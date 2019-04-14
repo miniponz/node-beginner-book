3485679bb774fd9b6a3f4df73d7aa10fd70048e6
Cara Ponzini <ponzini@gmail.com>
2019-04-14 14:55:16 -0700

    added exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,14 @@
+const exec = require('child_process').exec;
+
 function start() {
   console.log('request handler START was called');
+  let content = 'empty';
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while(new Date().getTime() < startTime + milliSeconds);
-  }
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
 
-  sleep(10000);
-  return 'Hello Start';
+  return content;
 }
 
 function upload() {
