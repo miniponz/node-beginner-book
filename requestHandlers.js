56d8c00fd38dede0441d777efedab35af4af0103
Cara Ponzini <ponzini@gmail.com>
2019-04-14 15:17:14 -0700

    added HTML to start

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,15 +1,25 @@
-const exec = require('child_process').exec;
+// const exec = require('child_process').exec;
 
 function start(res) {
   console.log('request handler START was called');
 
-  exec('find /', 
-    { timeout: 10000, maxBuffer: 20000 * 1024 },
-    function(error, stdout, stderr) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write(stdout);
-      res.end();  
-    });
+  const body = `
+  <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <form action="/upload" method="post">
+        <textarea name="text" rows="20" cols="60"></textarea>
+        <input type="submit" value="Submit text" />
+        </form>
+    </body>
+  </html>`;
+
+  res.writeHead(404, { 'Content-Type': 'text/html' });
+  res.write(body);
+  res.end();  
+
 }
 
 function upload(res) {
